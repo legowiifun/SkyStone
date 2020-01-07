509b040837303c856fbf27df69cdc2948f830453
legowiifun <arath5@gmail.com>
2020-01-07 17:48:57 -0600

    autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrobotixTestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrobotixTestAutonomous.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="Test Autonomous", group="Autonomous")
+@TeleOp(name="Move Forward Autonomous", group="Autonomous")
 
 public class TrobotixTestAutonomous extends LinearOpMode {
     public void runOpMode() {
@@ -15,25 +15,8 @@ public class TrobotixTestAutonomous extends LinearOpMode {
         Drive drive = new Drive(trobot);
 
         waitForStart();
-
-        distanceDrive(1, 30); //Drive forward 10 cm at 30% power
-
-        distanceDrive(1, -30); //Drive backwards 10 cm at 30% power
-
-
-
-    }
-
-    public void distanceDrive(double speed, double distance) {
-
-        Configure configure = new Configure(hardwareMap);
-        configure.initialize();
-        Trobot trobot = configure.getTrobot();
-
-        Drive drive = new Drive(trobot);
-
-        drive.move(speed, distance);
-        sleep(drive.getTime());
+        drive.driving(1,1,1);
+        sleep(1000);
         drive.stop();
     }
 }
