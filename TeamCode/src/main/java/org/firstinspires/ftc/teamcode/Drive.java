a878acfc2854516bd2bd41f5a9de3af6d1b559a4
legowiifun <arath5@gmail.com>
2020-01-21 16:34:43 -0600

    Class Hierarchy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -2,15 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 // imports
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 import java.lang.Math;
-import static java.lang.Thread.sleep;
 
 public class Drive {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trobot.java
@@ -1,13 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 //imports
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 public class Trobot {
     private DcMotor intakeRight = null;
